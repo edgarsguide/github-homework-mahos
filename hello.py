f8ec8ec4a3b42f4d4b2b124ee5a15cf8c6970aff
Maho <msasaki.a@gmail.com>
2018-07-16 13:31:46 -0500

    add varying greeting messages depending on time

--- hello.py
+++ hello.py
@@ -1,4 +1,19 @@
+import time
+hour = time.localtime().tm_hour
+
+# set greeting depending on time
+if hour > 4 and hour < 12:
+    greeting = "Morning,"
+elif hour == 12:
+    greeting = "What's for lunch"
+elif hour > 12 and hour < 18:
+    greeting = "Good afternoon,"
+elif hour >= 18:
+    greeting = "Good evening"
+else:
+    greeting = "It's bed time,"
+
 name = input("Hi, what's your name?")
 
-print("Hello {}!!!".format(name))
+print("{} {}!".format(greeting, name))
 
